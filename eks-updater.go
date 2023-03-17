762ddb34dbe7f8cc76da76b0b9d1150bc4524153
Jason McIntosh <jason.mcintosh@armory.io>
2023-03-17 11:52:36 -0500

    Some cleanup

--- eks-updater.go
+++ eks-updater.go
@@ -11,38 +11,38 @@ import (
 	"time"
 )
 
+/*
+	LONG TERM goals:
+	Update CLUSTER VERSION as well.   E.g. this can update the whole shebang saving a lot of time/headache.  We can validate things beforehand LIKE
+		- Make sure there are PDBs on resources like spinnaker first
+		- Verify nodes are on an n-1 release version first.  AND update nodes post upgrade kinda things
+*/
 func main() {
+
+	clusterName := utils.ValidateOrExit("cluster-name", "Cluster name REQUIRED")
+	//TODO: LOOK UP managed node groups instead of parameters... enhancement for later.  AND update multiple node groups sequentially would be a later thing
+	nodegroupName := utils.ValidateOrExit("nodegroup-name", "Node group name to update REQUIRED")
+	waitTimeForNodeUpdates := *flag.Int("nodegroup-wait-time", 120, "Time in minutes to wait for node group update to complete.  Defaults to 120 minutes")
+	addonsToUpdate := strings.Split(*flag.String("addons-to-update", "kube-proxy,coredns,vpc-cni,aws-ebs-csi-driver", "Comma separated list of adds on to updates.  Defaults to kube-proxy, coredns, vpc-cni, aws-ebs-csi-driver addons"), ",")
+
 	// Load the Shared AWS Configuration (~/.aws/config)
 	ctx := context.TODO()
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("ERROR: Unable to auth/get connected to AWS", err)
 	}
-
-	// Create an Amazon EKS Client
 	client := eks.NewFromConfig(cfg)
-	clusterName := utils.ValidateOrExit("cluster-name", "", "Cluster name REQUIRED")
-	addonsToUpdate := strings.Split(*flag.String("addons-to-update", "kube-proxy,coredns,vpc-cni,aws-ebs-csi-driver", "Comma separated list of adds on to updates.  Defaults to kube-proxy, coredns, vpc-cni, aws-ebs-csi-driver addons"), ",")
 
-	/*
-		LONG TERM goals:
-		Update CLUSTER VERSION as well.   E.g. this can updated the whole shebang saving a whole lot of time/headache.  We can validate things before hand LIKE
-			- Make sure there are PDB's on resources like spinnaker first
-			- Verify nodes are on a n-1 release version first.  AND update nodes post upgrade kinda things
-	*/
-
-	//TODO: LOOK UP managed node groups instead of parameters... enhancement for later
-	nodegroupName := utils.ValidateOrExit("nodegroup-name", "", "Cluster name REQUIRED")
-	waitForNodeUpdates := *flag.Int("nodegroup-wait-time", 120, "Time in minutes to wait for node group update to complete.  Defaults to 120 minutes")
+	log.Println("INFO: Starting updates...")
 	clusterInformation, _ := client.DescribeCluster(ctx, &eks.DescribeClusterInput{Name: clusterName})
-
-	updateError := updateClusterNodeGroup(client, ctx, clusterName, nodegroupName, waitForNodeUpdates)
+	updateError := updateClusterNodeGroup(client, ctx, clusterName, nodegroupName, waitTimeForNodeUpdates)
 	if updateError != nil {
-		log.Fatal("Unable to update cluster node group... ", updateError)
+		log.Fatal("ERROR: Unable to update cluster node group... ", updateError)
 	}
 	for _, addon := range addonsToUpdate {
 		updateAddon(client, ctx, clusterName, &addon, clusterInformation.Cluster.Version)
 	}
+	log.Println("INFO:  Updates complete!")
 
 }
 
@@ -74,7 +74,7 @@ func updateAddon(client *eks.Client, ctx context.Context, clusterName *string, a
 		AddonVersion:     &defaultVersion,
 		ResolveConflicts: "OVERWRITE",
 	})
-	log.Println("Updating addon " + *addonName + " id of update is:" + *response.Update.Id + " ... waiting for completion!")
+	log.Println("INFO: Updating addon " + *addonName + " id of update is:" + *response.Update.Id + " ... waiting for completion!")
 	waiter := eks.NewAddonActiveWaiter(client)
 	waitErr := waiter.Wait(ctx, &eks.DescribeAddonInput{
 		AddonName:   addonName,
@@ -91,7 +91,7 @@ func updateClusterNodeGroup(client *eks.Client, ctx context.Context, clusterName
 		log.Println("ERROR: Update call failed", err)
 		return err
 	}
-	log.Println("Upgrade job started... " + *version.Update.Id)
+	log.Println("INFO: Upgrade job started... " + *version.Update.Id)
 	waiter := eks.NewNodegroupActiveWaiter(client)
 	waitErr := waiter.Wait(ctx, &eks.DescribeNodegroupInput{ClusterName: clusterName, NodegroupName: nodegroupName}, time.Duration(waitForNodeUpdates)*time.Minute)
 	if waitErr != nil {
--- utilities/utilities.go
+++ utilities/utilities.go
@@ -10,9 +10,9 @@ func Strref(s string) *string {
 	return &s
 }
 
-func ValidateOrExit(parameterName string, defaultValue string, description string) *string {
+func ValidateOrExit(parameterName string, description string) *string {
 
-	value := flag.String(parameterName, defaultValue, description)
+	value := flag.String(parameterName, "", description)
 	if (len(*value)) == 0 {
 		log.Fatal("Invalid value for " + parameterName + " Must be set!")
 		os.Exit(1)
